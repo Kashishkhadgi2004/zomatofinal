a4805345ae0a683c267c835bcbfcc719d01293da
Ayush Khadgi <107793290+Ayushkhadgi@users.noreply.github.com>
2023-01-13 17:31:36 +0530

    zomato

---
+++ src/Component/Listing/listingApi.js
@@ -0,0 +1,55 @@
+import React,{Component} from 'react';
+import axios from 'axios';
+import './listing.css';
+import ListingDisplay from './listingDisplay';
+import CuisineFilter from '../Filters/cuisineFilter';
+import CostFilter from '../Filters/costFilter';
+import Header from '../../Header';
+
+const url = "https://zomapi-b2sk.onrender.com/restaurant?mealtype_id=";
+
+class ListingApi extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            restaurantList:''
+        }
+    }
+
+    setDataPerFilter = (data) => {
+        this.setState({restaurantList:data})
+    }
+
+    render(){
+        return(
+            <>
+                <Header/>
+                <div className="row">
+                    <div id="mainListing">
+                        <div id="filter">
+                            <center>
+                                <h3>Filter</h3>
+
+                            </center>
+                            <CuisineFilter mealId={this.props.match.params.mealId}
+                            restPerCuisine={(data) => {this.setDataPerFilter(data)}}/>
+                            <CostFilter  mealId={this.props.match.params.mealId}
+                            restPerCost={(data) => {this.setDataPerFilter(data)}}/>
+                        </div>
+                        <ListingDisplay listData={this.state.restaurantList}/>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    componentDidMount(){
+        let mealId = this.props.match.params.mealId;
+        sessionStorage.setItem('mealId',mealId);
+        axios.get(`${url}${mealId}`,{method:'GET'})
+        .then((res) => {this.setState({restaurantList:res.data})})
+    }
+}
+
+export default ListingApi;
---
+++ src/Routing.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
+import Home from './Component/Home/Home';
+import Listing from './Component/Listing/listingApi';
+import Details from './Component/Details/details';
+import PlaceOrder from './Component/Booking/placeOrder';
+import ViewOrder from './Component/Booking/viewOrder';
+import Login from './Component/Login/login';
+import Register from './Component/Login/register';
+
+const Routing = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/listing/:mealId" component={Listing}/>
+                    <Route path="/details" component={Details}/>
+                    <Route path="/placeOrder/:restName" component={PlaceOrder}/>
+                    <Route path="/viewBooking" component={ViewOrder}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                <Footer/>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default Routing;
