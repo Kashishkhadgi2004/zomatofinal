16c6343f4712f6e705188b9dc694cf03acccb059
Ayush Khadgi <107793290+Ayushkhadgi@users.noreply.github.com>
2023-09-29 17:37:36 +0530

    Update Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -18,7 +18,7 @@ const Routing = () => {
                     <Route path="/listing/:mealId" component={Listing}/>
                     <Route path="/details" component={Details}/>
                     <Route path="/placeOrder/:restName" component={PlaceOrder}/>
-                    <Route path="/viewBooking" component={ViewOrder}/>
+                    <Route path="/viewOrder" component={ViewOrder}/>
                     <Route path="/login" component={Login}/>
                     <Route path="/register" component={Register}/>
                 <Footer/>
